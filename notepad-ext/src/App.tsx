52aaed3980ff1954384adec910501f06c659c6bb
VEddie <edijose18@gmail.com>
2025-05-19 19:05:55 -0400

    Added a short to do list for tomorrow

--- notepad-ext/src/App.tsx
+++ notepad-ext/src/App.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 
 function App() {
     const [selectedText, setSelectedText] = useState<string>();
+    // This state needs to be shared across components for highlighting, use React Context.
 
     useEffect(() => {
         document.addEventListener('selectionchange', () => {
@@ -14,11 +15,17 @@ function App() {
     }, []);
 
     return (
+        // Needs a container for the UI.
+        // Needs a character limit.
+        // Add an offcanvas to view notes (?)
         <VStack>
             <FormatOptions />
             <NoteField />
             <Text>Currently highlighted: {selectedText}</Text>
         </VStack>
+
+        // To do later: Add basic API to save users & notes.
+        // Allow text files to be loaded into the app.
     );
 }
 
