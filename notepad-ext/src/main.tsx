1535e4dd9b8480fc1d1396f033627b2cfb6d5ae8
VEddie <edijose18@gmail.com>
2025-05-19 16:50:11 -0400

    Installed Chakra UI & Bootstrap

--- notepad-ext/src/main.tsx
+++ notepad-ext/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from './components/ui/provider.tsx';
+import 'bootstrap/dist/css/bootstrap.css';
+import App from './App.tsx';
+
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <Provider>
+            <App />
+        </Provider>
+    </StrictMode>
+);
