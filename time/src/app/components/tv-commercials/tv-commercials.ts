d8f95085c573c42cf246403179ef1404b372973f
VEddie <edijose18@gmail.com>
2025-06-19 22:30:30 -0400

    Needs a CSS fix for resizing, adding actual video ads tomorrow

--- time/src/app/components/tv-commercials/tv-commercials.ts
+++ time/src/app/components/tv-commercials/tv-commercials.ts
@@ -1,11 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'tv-commercials',
-  imports: [],
-  templateUrl: './tv-commercials.html',
-  styleUrl: './tv-commercials.css'
+    selector: 'tv-commercials',
+    imports: [CommonModule],
+    templateUrl: './tv-commercials.html',
+    styleUrl: './tv-commercials.css',
 })
 export class TvCommercials {
+    timeLeft: number = 5;
+    interval: any;
 
+    currentSrc: number = 0;
+    videoInterval: any;
+    videoList = [
+        '/videos/video1.jpg',
+        '/videos/video2.jpg',
+        '/videos/video3.jpg',
+        '/videos/video4.jpg',
+    ]
+
+    startTimer() {
+        this.interval = setInterval(() => {
+            if (this.timeLeft > 0) this.timeLeft--;
+            else this.pauseTimer();
+        }, 1000);
+
+        this.videoInterval = setInterval(() => {
+            console.log('random');
+            this.currentSrc = Math.floor(Math.random() * 4);
+        }, 2000)
+    }
+
+    pauseTimer() {
+        clearInterval(this.interval);
+    }
 }
+
+//
