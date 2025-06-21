c6711189ced1ae31852c874e826287d18f73547b
VEddie <edijose18@gmail.com>
2025-06-20 22:14:32 -0400

    Redesigned canvas logic

--- time/src/app/components/battery-charge/battery-charge.ts
+++ time/src/app/components/battery-charge/battery-charge.ts
@@ -12,7 +12,9 @@ export class BatteryCharge {
     private canvasRef!: ElementRef<HTMLCanvasElement>;
     private ctx!: CanvasRenderingContext2D;
     public isBrowser: boolean;
+    chargeValue = 10;
 
+    // Should fix the "Is not yet implemented" error but gets a TypeError instead
     constructor(@Inject(PLATFORM_ID) platformId: Object) {
         this.isBrowser = isPlatformBrowser(platformId);
     }
@@ -23,9 +25,27 @@ export class BatteryCharge {
         this.drawSample();
     }
 
-    private drawSample(): void {
-        this.ctx.fillStyle = '#e05c4c';
+    sleep(delay: number) {
+        return new Promise((resolve) => setTimeout(resolve, delay));
+    }
+
+    private async drawSample(): Promise<void> {
         // Min 10 width, Max 140 width
-        this.ctx.fillRect(76, 240, 10, 59);
+        // Yellow: #FADF63 @ 50% progress
+        // Green: #57C75C @ 75% progress
+        this.ctx.lineWidth = 3;
+        this.ctx.strokeStyle = 'white';
+        this.ctx.strokeRect(75, 240, 140, 59);
+
+        while (this.chargeValue <= 136) {
+            if (this.chargeValue < 70) this.ctx.fillStyle = '#E05C4C';
+            else if (this.chargeValue < 100) this.ctx.fillStyle = '#FADF63';
+            else this.ctx.fillStyle = '#57C75C';
+
+            this.ctx.fillRect(77, 241, this.chargeValue, 56);
+
+            await this.sleep(25);
+            this.chargeValue += 9;
+        }
     }
 }
