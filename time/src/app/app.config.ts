e188bd6cf22880ae33205391c42a17afe19ef644
VEddie <edijose18@gmail.com>
2025-06-28 01:17:43 -0400

    HotToast added

--- time/src/app/app.config.ts
+++ time/src/app/app.config.ts
@@ -4,12 +4,17 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHotToastConfig } from '@ngneat/hot-toast';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes), provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
+    provideHotToastConfig({
+        duration: 3000,
+        dismissible: true
+    })
   ]
 };
--- time/src/app/pages/login/login.ts
+++ time/src/app/pages/login/login.ts
@@ -1,28 +1,38 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Inject, Input, Output } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { UserService } from '../../services/user-service';
 import { fetchUserData } from '../../utilities/functions';
 import { Router, RouterModule } from '@angular/router';
+import { HotToastService } from '@ngneat/hot-toast';
 
 @Component({
-  selector: 'login',
-  imports: [FormsModule, RouterModule],
-  templateUrl: './login.html',
-  styleUrl: './login.css'
+    selector: 'login',
+    imports: [FormsModule, RouterModule],
+    templateUrl: './login.html',
+    styleUrl: './login.css',
 })
 export class Login {
-    constructor(private userService: UserService, private router: Router) {}
+    toastRef: any;
+
+    constructor(
+        private userService: UserService,
+        private toast: HotToastService,
+        private router: Router
+    ) {}
 
     @Output() userEvent = new EventEmitter();
 
     onSubmit(formData: NgForm) {
-        this.userService.getAll().subscribe(users => {
-            let user = fetchUserData(users, formData.value)
+        this.userService.getAll().subscribe((users) => {
+            let user = fetchUserData(users, formData.value);
             // Switch to a toaster
-            if(user)
-                this.router.navigate(['/component-list']);
-            else
-                console.log('Invalid user.');
-        })
+            if (user) {
+                this.toast.success('Welcome');
+                // const toasty = new Toasty();
+                // toasty.success('You did the thing');
+            }
+            //this.router.navigate(['/component-list']);
+            else this.toast.error('Invalid user.');
+        });
     }
 }
