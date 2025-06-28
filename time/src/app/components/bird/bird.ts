4fbae0a5bcae7e4a9e6181c92ebf7162709aa151
VEddie <edijose18@gmail.com>
2025-06-28 17:50:35 -0400

    Bird component fully done

--- time/src/app/components/bird/bird.ts
+++ time/src/app/components/bird/bird.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { getTimer } from '../../utilities/functions';
 
 @Component({
   selector: 'bird',
@@ -12,27 +13,41 @@ export class Bird {
     @ViewChild('birdNest', { static: false}) birdNest!: ElementRef<HTMLImageElement>;
     @ViewChild('birdInNest', { static: false}) birdInNest!: ElementRef<HTMLImageElement>;
     ctx!: CanvasRenderingContext2D;
-    position = 600;
     flyInterval!: NodeJS.Timeout; 
+    position = 600;
+    timer = 20;
+    maxTimer = this.timer;
 
     ngAfterViewInit() {
         this.ctx = this.canvasRef.nativeElement.getContext('2d')!;
-        this.ctx.drawImage(this.flyingBird.nativeElement, this.position, 250, 64, 64);
-        this.ctx.drawImage(this.birdNest.nativeElement, 125, 250, 64, 64);
+        this.flyingBird.nativeElement.addEventListener('load', () => {
+            this.ctx.drawImage(this.flyingBird.nativeElement, this.position, 250, 64, 64);
+        });
+
+        this.birdNest.nativeElement.addEventListener('load', () => {
+            this.ctx.drawImage(this.birdNest.nativeElement, 125, 250, 64, 64);
+        });
     }
 
      fly() {
         this.flyInterval = setInterval(() => {
             this.ctx.clearRect(195, 180, 600, 200);
             this.ctx.drawImage(this.flyingBird.nativeElement, this.position, 250, 64, 64);
-            this.position--;
 
+            this.ctx.fillStyle = '4d6409'
+            this.ctx.font = '24px Verdana';
+            this.ctx.clearRect(200, 50, 200, 50);
+            this.ctx.fillText(getTimer(this.timer), 240, 85);
+
+            this.position -= 400 / this.maxTimer;
+            this.timer--;
+
+            // Bird has to travel 400 pixels.
             if(this.position < 200) {
                 clearInterval(this.flyInterval)
                 this.ctx.clearRect(125, 180, 600, 200);
                 this.ctx.drawImage(this.birdInNest.nativeElement, 125, 250, 64, 64);
-                return;
             } 
-        }, 10)
+        }, 1000)
     }
 }
